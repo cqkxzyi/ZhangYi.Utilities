32e13a8adee19c5984e4ca994dedd1c0ef3563e8
zhangyi <kxyi-lover@163.com>
2019-11-20 19:05:16 +0800

    Mencached开发

---
+++ doNet/站点/MvcObject/Controllers/TestMencacheController.cs
@@ -0,0 +1,26 @@
+﻿using MvcObject.通用;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcObject.Controllers
+{
+    public class TestMencacheController : Controller
+    {
+        //Mencache缓存测试
+        public ActionResult Index()
+        {
+            var obj=MemcacheHelper.Get("zhangyi");
+            if (obj == null)
+            {
+                MemcacheHelper.Set("zhangyi", "我的名字叫张毅");
+            }
+      
+
+
+            return View();
+        }
+    }
+}
---
+++ doNet/站点/MvcObject/通用/MemcacheHelper.cs
@@ -0,0 +1,99 @@
+﻿using Memcached.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcObject.通用
+{
+    public class MemcacheHelper
+    {
+        private static readonly MemcachedClient mc = null;
+        static MemcacheHelper()
+        {
+            string[] serverlist = { "192.168.10.61:11212" };//一定要将地址写到Web.config文件中。
+            //初始化池
+            SockIOPool pool = SockIOPool.GetInstance();
+            pool.SetServers(serverlist);
+
+            //各服务器之间负载均衡的设置比例
+            pool.SetWeights(new int[] { 1 });
+            //初始化时创建连接数
+            pool.InitConnections = 3;
+            //最小连接数
+            pool.MinConnections = 3;
+            //最大连接数
+            pool.MaxConnections = 5;
+            //连接的最大空闲时间，下面设置为6个小时（单位ms），超过这个设置时间，连接会被释放掉
+            pool.MaxIdle = 1000 * 60 * 60 * 6;
+            //socket连接的超时时间，下面设置表示不超时（单位ms），即一直保持链接状态
+            pool.SocketConnectTimeout = 0;
+            //通讯的超市时间，下面设置为3秒（单位ms），.Net版本没有实现
+            pool.SocketTimeout = 1000 * 3;
+            //维护线程的间隔激活时间，下面设置为30秒（单位s），设置为0时表示不启用维护线程
+            pool.MaintenanceSleep = 30;
+            //设置SocktIO池的故障标志
+            pool.Failover = true;
+            //是否对TCP/IP通讯使用nalgle算法，.net版本没有实现
+            pool.Nagle = false;
+            //socket单次任务的最大时间（单位ms），超过这个时间socket会被强行中端掉，当前任务失败。
+            pool.MaxBusy = 1000 * 10;
+
+
+
+            pool.Initialize();
+
+            // 获得客户端实例
+            mc = new MemcachedClient();     
+            //是否启用压缩数据：如果启用了压缩，数据压缩长于门槛的数据将被储存在压缩的形式
+            mc.EnableCompression = false;
+            //压缩设置，超过指定大小的都压缩 
+            //mc.CompressionThreshold = 1024 * 1024;  
+        }
+
+        /// <summary>
+        /// 向Memcache中写数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void Set(string key, object value)
+        {
+            mc.Set(key, value);
+        }
+
+        /// <summary>
+        ///  向Memcache中写数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="time">过期时间</param>
+        public static void Set(string key, object value, DateTime time)
+        {
+            mc.Set(key, value, time);
+        }
+
+        /// <summary>
+        /// 获取Memcahce中的数据
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static object Get(string key)
+        {
+            return mc.Get(key);
+        }
+
+        /// <summary>
+        /// 删除Memcache中的数据
+        /// </summary>
+        /// <param name="key"></param>
+        public static bool Delete(string key)
+        {
+            if (mc.KeyExists(key))
+            {
+                return mc.Delete(key);
+            }
+            return false;
+        }
+
+    }
+}
