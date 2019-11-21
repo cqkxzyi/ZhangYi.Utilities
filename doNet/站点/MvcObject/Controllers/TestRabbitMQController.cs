f48f9bc9efe9f6f8724bc4b9c895803d32f6ab4d
zhangyi <kxyi-lover@163.com>
2019-11-21 22:48:00 +0800

    RabbitMQ学习

---
+++ doNet/站点/MvcObject/Controllers/TestRabbitMQController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcObject.Controllers
+{
+    /// <summary>
+    /// RabbitMQ测试
+    /// 
+    /// ExchangeType有direct、Fanout和Topic三种，不同类型的Exchange路由的行为是不一样的
+    /// 
+    /// 
+    /// 
+    /// 
+    /// </summary>
+    public class TestRabbitMQController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
