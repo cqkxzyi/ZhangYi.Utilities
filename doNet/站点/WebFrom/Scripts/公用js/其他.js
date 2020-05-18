fffc09bab5358db6292f598d3e5b035d121ed4e6
zhangyi <kxyi-lover@163.com>
2020-05-18 17:15:45 +0800

    company

--- doNet/站点/WebFrom/Scripts/公用js/其他.js
+++ doNet/站点/WebFrom/Scripts/公用js/其他.js
@@ -24,16 +24,33 @@ params = $.extend(true, params, options);
 //●查找最近祖先元素并删除：
 $(当前对象).closest('查找').remove();
 
-//●返回数组中指定元素的索引值
-var arr = [4, "Pete", 8, "John"];
-$.inArray(4, arr);//输出0
-
 //●显示延迟关闭
 $('.error_tips').show().delay(1500).fadeOut();
-//主动触发事件
+
+//●输入框改变事件，优于blur
+ $('#TransactionPrice').bind('input porpertychange', function(){});
+
+//●主动触发事件
+ $('#TransactionPrice').trigger("porpertychange");
+ 
+//●判断某个元素在数组中的位置
+var arr = [4, "Pete", 8, "John"];
+$.inArray(4, arr);//输出0
+ 
+//●删除数组中的某个元素
+ array.splice(0,1);
+ 
+//●判断数组是否为null
+ if (array === undefined || array.length == 0) 
+ 
+//●如果该对象存在其他click事件先解绑事件
+ $("[name='delete']").off("click");
+ 
+//●只绑定一次事件
+ $("[name='download']").one("click", function () 
+ 
+//●主动触发事件
 $('#name').trigger("porpertychange");
-
-
 
 
 //===========================js=========================
